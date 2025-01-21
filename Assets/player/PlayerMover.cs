52cc931bbbaa04ef505811879a473d9553b0c361
CCColda <cccolda@gmail.com>
2025-01-21 14:54:36 +0100

    Add new graphics to player animations #2  - add uppercut animation state  - reorganize player movement code (prep. for damage movement)  - add placeholder for enemy  - add attackable tag  - add empty attackable script

--- Assets/player/PlayerMover.cs
+++ Assets/player/PlayerMover.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMover : MonoBehaviour
@@ -9,46 +10,73 @@ public class PlayerMover : MonoBehaviour
     public float jumpForce;
     public float walksSpeed;
     public BoxCollider2D col;
+
+    /// <summary>
+    /// Azt tárolja, melyik vízszintes irányba néz a player. -1: balra, 1: jobbra, 0: nem def.
+    /// Ez alapján lesz meghatározva a dash iránya.
+    /// </summary>
+    private int _facingDirection = 1;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // down-ra vagy s-re átesik, ha földön van
         if ((Input.GetKeyDown(KeyCode.DownArrow) || (Input.GetKeyDown(KeyCode.S)) && ground.tag == "openFloor" && touchesGround))
         {
             col.isTrigger = true;
-
         }
+
+        // space-re uppercut-ol, ha földön van
+        // TODO: damage
         if (Input.GetKeyDown(KeyCode.Space) && touchesGround)
         {
             rb.linearVelocityY = 0f;
             rb.AddForce(Vector2.up * jumpForce);
             touchesGround = false;
+            GetComponent<Animator>().SetBool("isPerformingUppercut", true);
         }
-        rb.linearVelocityX = Input.GetAxis("Horizontal") * walksSpeed;
-        if (rb.linearVelocityX < 0f)
-        {
 
-            GetComponent<SpriteRenderer>().flipX = true;
-        }
-        if (rb.linearVelocityX > 0f)
+        rb.linearVelocityX = Input.GetAxis("Horizontal") * walksSpeed;
+
+        UpdateMovementStatus();
+
+        // TODO: dash
+
+        UpdateAnimation();
+    }
+
+    /// <summary>
+    /// Frissíti a belső mozgással kapcsolatos változókat a physics engine alapján.
+    /// </summary>
+    private void UpdateMovementStatus()
+    {
+        _facingDirection = Math.Sign(rb.linearVelocityX);
+
+        GetComponent<SpriteRenderer>().flipX = (_facingDirection == (-1));
+    }
+
+    /// <summary>
+    /// Frissíti az animátor állapotát.
+    /// </summary>
+    private void UpdateAnimation()
+    {
+        // isWalking igaz lesz, ha a vízszintes sebesség nem nulla
+        GetComponent<Animator>().SetBool("isWalking", (Mathf.Abs(rb.linearVelocityX) > 0f));
+
+        // szűnjön meg az uppercut animáció, amikor elkezdünk esni, vagy földet érünk
+        if (rb.linearVelocityY < 0f || touchesGround)
         {
-            GetComponent<SpriteRenderer>().flipX = false;
-        }
-        if (Mathf.Abs(rb.linearVelocityX) > 0f)
-        {
-            GetComponent<Animator>().SetBool("isWalking", true);
-        }
-        else
-        {
-            GetComponent<Animator>().SetBool("isWalking", false);
+            GetComponent<Animator>().SetBool("isPerformingUppercut", false);
         }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject != gameObject)
@@ -59,15 +87,12 @@ public class PlayerMover : MonoBehaviour
             ground = collision.gameObject;
             col.enabled = true;
         }
-   
-
-
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject != gameObject)
         {
-
             touchesGround = false;
             //GetComponent<Animator>().SetBool("groundTouch", false); //will be important if we have jump animation.
             col.sharedMaterial.friction = 0f;
