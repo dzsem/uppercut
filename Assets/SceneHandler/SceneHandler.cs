ffe08a57a72996e3802531a79fb02943f2b714d3
Szmoleniczki Ákos <szmoleniczki.akos@edu.bme.hu>
2025-01-26 19:03:46 +0100

    scene loading works but has a fatal flaw which needs a workaround and I need some rest

--- Assets/SceneHandler/SceneHandler.cs
+++ Assets/SceneHandler/SceneHandler.cs
@@ -23,25 +23,14 @@ public class SceneHandler : MonoBehaviour {
         // SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
-        // Debug.Log("OnSceneLoaded: " + scene.name);
-        // Debug.Log(mode);
-
-        // SceneManager.SetActiveScene(scene);
-        // GameObject root = SceneManager.GetActiveScene().GetRootGameObjects()[0];
-        // Debug.Log(root);
-        // if (root != null) {
-        //     root.transform.position = new Vector3(40f, 0f, 0f);
-        // }
-
-        // SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-
     // Update is called once per frame
     void Update() {
-        
+        LoadScenes();
     }
 
+    /**
+    *   Legenerálja a mapot a meghatározott scenekből és paraméterekből, amiket az asset menuben meg lehet adni.
+    */
     private List<string> GenerateMap() {
         List<string> map = new List<string>();
         map.Add(startScene);
@@ -55,6 +44,9 @@ public class SceneHandler : MonoBehaviour {
         return map;
     }
 
+    /**
+    *   Karbantartja a _loadedScenes-t és betölti a megfelelő sceneket
+    */
     private void LoadScenes() {
         _loadedScenes.Clear();
         foreach (int sceneIndex in _unloadedScenes) {
@@ -62,11 +54,35 @@ public class SceneHandler : MonoBehaviour {
             float sceneEndX = (sceneIndex * sceneWidth) + (sceneWidth / 2);
             Debug.Log($"{_map.ElementAt(sceneIndex)}: {sceneStartX}, {sceneEndX}");
 
-            if (Math.Abs(player.transform.position.x - sceneStartX) < sceneWidth || Math.Abs(player.transform.position.x - sceneEndX) < sceneWidth) {
-                _loadedScenes.Add(sceneIndex);
+            bool isPlayerInLoadRange = Math.Abs(player.transform.position.x - sceneStartX) < sceneWidth || Math.Abs(player.transform.position.x - sceneEndX) < sceneWidth; // Ez a formula, ami alapján kiszámolom, hogy betöltési rangeben van-e vagy sem. Mindig három scenet tölt be: amiben a player áll, az előtte lévőt és az utána lévőt (ha vannak ilyenek)
+            if (isPlayerInLoadRange) {
                 Debug.Log($"{_map.ElementAt(sceneIndex)} is in load range");
+                _loadedScenes.Add(sceneIndex);
+                _unloadedScenes.Remove(sceneIndex);
+                SceneManager.LoadSceneAsync(_map.ElementAt(sceneIndex), LoadSceneMode.Additive).completed += _ => {
+                    OnSceneLoaded(SceneManager.GetSceneByName(_map.ElementAt(sceneIndex)), sceneIndex, LoadSceneMode.Additive); // idk ezt a delegációs faszságot a chatgőt főzte, mert nem tudtam a SceneManager.sceneLoaded eventtel megoldani normálisan
+                };
             }
         }
+    }
+
+    /**
+    *   Ez a callback, akkor hívódik meg, amikor egy scene betöltése befejeződik és csak a jó helyre pozíciónálja a scenet.
+    */
+    private void OnSceneLoaded(Scene scene, int sceneIndex, LoadSceneMode mode) {
+        Debug.Log("OnSceneLoaded: " + scene.name);
+        Debug.Log(mode);
+        SceneManager.SetActiveScene(scene);
+        GameObject root = SceneManager.GetActiveScene().GetRootGameObjects()[0];
+        Debug.Log(root);
+
+        if (root == null) {
+            return; // ha nincs root, akkor szar a scene setup és nem csinál semmit
+        }
+
+        float sceneCenterX = sceneIndex * sceneWidth; // ez a formula ami alapján a scenek helye ki van számolva
+        Debug.Log($"{sceneIndex}: {sceneCenterX}");
+        root.transform.position = new Vector3(sceneCenterX, 0f, 0f); // elmozdítja a scenet a jó helyre
     }
 
     private void UnloadScenes() {
