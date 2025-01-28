f4db5bd3c00d3943e6fb8510cd8976c24a5ceb37
Szmoleniczki Ákos <szmoleniczki.akos@edu.bme.hu>
2025-01-28 11:01:30 +0100

    added offsetDistanceY

--- Assets/CameraSystem/MainCamera.cs
+++ Assets/CameraSystem/MainCamera.cs
@@ -7,8 +7,8 @@ public class MainCamera : MonoBehaviour
     void Start()
     {
         _fallTimer = 0f;
-        _cameraOffsetX = offsetDistance;
-        _cameraOffsetY = offsetDistance * 1.5f;
+        _cameraOffsetX = offsetDistanceX;
+        _cameraOffsetY = offsetDistanceY;
         _targetPoint = new Vector3(player.transform.position.x + _cameraOffsetX, player.transform.position.y + _cameraOffsetY, -10f);
         transform.position = _targetPoint;
     }
@@ -19,10 +19,10 @@ public class MainCamera : MonoBehaviour
         // Ezekkel kicsit eltolom abba az irányba a kamerát, amerre a karakter néz, hogy több látszódjon a pályából abba az irányba.
         if (player.transform.rotation.y == 0) { // facing right
             // _cameraOffsetX = Mathf.Lerp(_cameraOffsetX, offsetDistance, offsetSmoothing * Time.deltaTime);
-            _cameraOffsetX = offsetDistance;
+            _cameraOffsetX = offsetDistanceX;
         } else { // facing left
             // _cameraOffsetX = Mathf.Lerp(_cameraOffsetX, -offsetDistance, offsetSmoothing * Time.deltaTime);
-            _cameraOffsetX = -offsetDistance;
+            _cameraOffsetX = -offsetDistanceX;
         }
 
         // Esésnél kicsit lentebb néz a kamera, hogy a játékos jobban lássa, hogy hova fog érkezni
@@ -31,11 +31,11 @@ public class MainCamera : MonoBehaviour
             _fallTimer += Time.deltaTime;
             if (_fallTimer >= fallDelay) {
                 // _cameraOffsetY = Mathf.Lerp(_cameraOffsetY, -2 * cameraOffsetDistance, cameraOffsetSpeed * Time.deltaTime);
-                _cameraOffsetY = -2 * offsetDistance;
+                _cameraOffsetY = -2 * offsetDistanceY;
             }
         } else {
             _fallTimer = 0f;
-            _cameraOffsetY = offsetDistance * 1.5f;
+            _cameraOffsetY = offsetDistanceY;
         }
 
         // A kamera ugrásnál csak akkor vált pozíciót, ha a játékos már leérkezett, így könnyebb ugrani szerintem
@@ -50,8 +50,9 @@ public class MainCamera : MonoBehaviour
     }
 
     public PlayerMover player;
-    public float offsetDistance = 2f;
-    public float offsetSmoothing = 3.5f;
+    public float offsetDistanceX = 3.5f;
+    public float offsetDistanceY = 3f;
+    public float offsetSmoothing = 5f;
     public float fallDelay = 0.5f;
     private float _fallTimer;
     private Vector3 _targetPoint;
