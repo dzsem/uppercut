c06b18b82e475dc879cf7796fad5d0100a0b68d4
CCColda <cccolda@gmail.com>
2025-01-29 16:45:55 +0100

    Cleanup, reimportok

--- Assets/Attackable/BreakableWall/BreakableWall.cs
+++ Assets/Attackable/BreakableWall/BreakableWall.cs
@@ -4,19 +4,7 @@ public class BreakableWall : MonoBehaviour
 {
     public BoxCollider2D physicsCollider;
     public Animator animator;
-
     public bool isBroken = false;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public void OnHit(int damage)
     {
