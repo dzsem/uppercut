48992cc38276bca6006e57f60bf74cbd41273d41
CCColda <cccolda@gmail.com>
2025-01-30 18:11:11 +0100

    Add door SFX #24

--- Assets/SpawnRoom/SpawnRoomDoor/SpawnRoomDoor.cs
+++ Assets/SpawnRoom/SpawnRoomDoor/SpawnRoomDoor.cs
@@ -5,6 +5,7 @@ public class SpawnRoomDoor : MonoBehaviour
     public BoxCollider2D physicsCollider;
     public Animator animator;
     public SpriteRenderer sprite;
+    public AudioSource soundEmitter;
     public bool isBroken = false;
 
     [SerializeField] private Vector2 _openDoorTextureOffset = new Vector2(0.35f, 0.0f);
@@ -12,6 +13,7 @@ public class SpawnRoomDoor : MonoBehaviour
     public void OnHit(int damage)
     {
         isBroken = true;
+        soundEmitter.Play();
         transform.Translate(new Vector3(_openDoorTextureOffset.x, _openDoorTextureOffset.y, 0.0f));
         animator.SetBool("isDoorBroken", true);
     }
