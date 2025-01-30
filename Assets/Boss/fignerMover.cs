7e629e09fb8a3c58aa7a402422e0f3772f559db5
Miklós <recsegekecsege@gmail.com>
2025-01-30 18:15:42 +0100

    boss-játékos kezdetleges interakció

--- Assets/Boss/fignerMover.cs
+++ Assets/Boss/fignerMover.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor.ShaderGraph.Internal;
+﻿using Unity.VisualScripting;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class fignerMover : MonoBehaviour
@@ -22,8 +23,8 @@ public class fignerMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         firstHit = Physics2D.Raycast((body.transform.position + elevationOffset), Vector2.down);
-
 
         if (Mathf.Sign(body.GetComponent<Rigidbody2D>().linearVelocityX) >= 0)
         {
@@ -37,25 +38,28 @@ public class fignerMover : MonoBehaviour
         {
             shouldMove = true;
         }
-        if (Vector3.Distance(transform.position, body.transform.position + elevationOffset + new Vector3(_directionalOffset, 0, 0)) < 0.1)
+        if (Vector3.Distance(transform.position, body.transform.position + elevationOffset + new Vector3(_directionalOffset, 0, 0)) < 0.1 )
         {
             shouldMove = false;
         }
         if (shouldMove)
         {
+          
             transform.position = Vector2.MoveTowards(transform.position, body.transform.position + elevationOffset + new Vector3(_directionalOffset, 0, 0), speed *Time.deltaTime);
         }
         else
         {
             RaycastHit2D hit;
             hit = Physics2D.Raycast(transform.position, Vector2.down);
-         
+            
             
             if (hit.distance > 0.1)
             {
                 transform.position = Vector2.MoveTowards(transform.position, hit.point, speed * Time.deltaTime);
-                
+
             }
+ 
+
            
         }
        
